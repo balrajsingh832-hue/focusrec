99cef5c5a54e928f4dec01a1d33f4b231d5de56a
balrajsingh832-hue <balrajsingh832@gmail.com>
2026-05-24 07:26:53 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,19 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron');
-
 contextBridge.exposeInMainWorld('electronAPI', {
   getSources: () => ipcRenderer.invoke('get-sources'),
-  getSavePath: (filename) => ipcRenderer.invoke('get-save-path', filename),
-  saveVideo: (data) => ipcRenderer.invoke('save-video', data),
-  processVideo: (data) => ipcRenderer.invoke('process-video', data),
-  getTempPath: (filename) => ipcRenderer.invoke('get-temp-path', filename),
-  deleteFile: (filePath) => ipcRenderer.invoke('delete-file', filePath),
-  showOverlay: () => ipcRenderer.send('show-overlay'),
-  hideOverlay: () => ipcRenderer.send('hide-overlay'),
-  updateCursor: (pos) => ipcRenderer.send('update-cursor', pos),
+  getSavePath: (f) => ipcRenderer.invoke('get-save-path', f),
+  saveVideo: (d) => ipcRenderer.invoke('save-video', d),
   windowMinimize: () => ipcRenderer.send('window-minimize'),
   windowMaximize: () => ipcRenderer.send('window-maximize'),
   windowClose: () => ipcRenderer.send('window-close'),
-  onCursorPos: (cb) => ipcRenderer.on('cursor-pos', (e, pos) => cb(pos)),
-  onOverlayShow: (cb) => ipcRenderer.on('overlay-show', cb),
-  onOverlayHide: (cb) => ipcRenderer.on('overlay-hide', cb),
 });
